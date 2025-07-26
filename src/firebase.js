aa611f1a10a4f5250ac3ea3232b8e2841f023bf9
bharath-pypractice <bharathpraveenkumar2004@gmail.com>
2025-07-26 09:35:07 +0530

    firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,23 +1,20 @@
-// firebase.js
+// src/firebase.js
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAk4wxik1MgrqtyEOY8anxAZ3fsTSTpbHA",
-  authDomain: "healx-76c40.firebaseapp.com",
-  projectId: "healx-76c40",
-  storageBucket: "healx-76c40.appspot.com", 
-  messagingSenderId: "511124876421",
-  appId: "1:511124876421:web:66163b92482c68923a4dec"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// ✅ Export services
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
